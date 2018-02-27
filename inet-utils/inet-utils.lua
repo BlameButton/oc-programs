f48b6f5b8693793d34105402d63a38997813d3c5
Bram Ceulemans <bramceulemans@me.com>
2018-02-27 11:52:50 +0100

    Added post and get functions

--- inet-utils/inet-utils.lua
+++ inet-utils/inet-utils.lua
@@ -1,5 +1,23 @@
 local inet = require("internet")
 
-local function hello()
-  print("Hello, World")
+global function get(url)
+  local req = inet.request(url)
+  local body = ""
+  if (req) then
+    for line in req do
+      body = body .. line .. "\n"
+    end
+  end
+  return body
+end
+
+global function post(url, data)
+  local req = inet.request(url, data)
+  local body = ""
+  if (req) then
+    for line in req do
+      body = body .. line .. "\n"
+    end
+  end
+  return body
 end
